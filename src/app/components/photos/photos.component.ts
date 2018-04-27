6da99cc97699be855f88128e81451fb6218866aa
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-04-27 15:20:13 -0500

    photos component created

---
+++ src/app/components/photos/photos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector:'app-photos',
+    templateUrl:'./photos.component.html'
+})
+
+export class PhotosComponent{
+    constructor(){
+        
+    }
+}
