00c4cd0729d5af3917ef4ad2b8e9142642320938
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-04-27 15:14:32 -0500

    routes created

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes, RouterModule } from '@angular/router'
+import { PhotosComponent } from './components/photos/photos.component'
+import { UploadComponent } from './components/upload/upload.component'
+
+const APP_ROUTES:Routes = [
+    {
+        path:'photos', component:PhotosComponent,
+    },
+    {
+        path:'upload', component:UploadComponent
+    },
+    {
+        path:'**', pathMatch:'full', redirectTo:'photos'
+    }
+]
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES)
