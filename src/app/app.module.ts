6537538fd118f84df5dda0c926c4c452f2437fbf
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-05-07 14:15:52 -0500

    Firebase Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,36 @@ import { AppComponent } from './app.component';
 
 import { PhotosComponent } from './components/photos/photos.component'
 import { UploadComponent } from './components/upload/upload.component'
+import { PhotosUploader }  from './services/photos-uploader.service'
 
 import { APP_ROUTING }     from './app.routes'
+import { NgDropFilesDirective } from './directives/ng-drop-files.directive'
+
+//FireBase
+import { AngularFireModule } from 'angularfire2'
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth'
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [
     AppComponent,
     PhotosComponent,
-    UploadComponent
+    UploadComponent,
+    NgDropFilesDirective
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING
+    APP_ROUTING,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [
+    PhotosUploader
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
